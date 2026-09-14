aa5ba389b642d1f2a5de37bbd285e46218147111
Janin5 <antonia.janin2002@gmail.com>
2026-09-14 20:08:53 +0100

    working on the booking component

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,12 +1,26 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogClose,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 import { StylistService } from '../../services/stylist.service';
 import { Slot } from '../../models/slot';
+import { BehaviorSubject, switchMap } from 'rxjs';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-booking',
-  imports: [],
+  imports: [
+    MatIcon,
+    CommonModule,
+    MatIconButton,
+    MatDialogClose,
+    MatDialogModule,
+  ],
   templateUrl: './booking.component.html',
   styleUrl: './booking.component.css',
 })
@@ -15,6 +29,9 @@ export class BookingComponent implements OnInit {
   selectedSlot: Slot | null = null;
   displayedMonth: Date = new Date();
   availableDates: string[] = [];
+  weekDays: string[] = ['l', 'ma', 'mi', 'j', 'v', 's', 'd'];
+
+  private monthChange = new BehaviorSubject<Date>(new Date());
 
   constructor(
     @Inject(MAT_DIALOG_DATA)
@@ -27,14 +44,27 @@ export class BookingComponent implements OnInit {
     this.loadAvailability();
   }
 
-  loadAvailability() {
-    this.stylistService
-      .getAvailability(
-        this.data.stylistId,
-        this.data.procedureId,
-        this.displayedMonth.getMonth() + 1,
-        this.displayedMonth.getFullYear(),
+  loadAvailability(): void {
+    this.monthChange
+      .pipe(
+        switchMap((month) =>
+          this.stylistService.getAvailability(
+            this.data.stylistId,
+            this.data.procedureId,
+            month.getMonth() + 1,
+            month.getFullYear(),
+          ),
+        ),
       )
       .subscribe((dates) => (this.availableDates = dates));
+  }
+
+  onMonthSelected(month: Date): void {
+    this.displayedMonth = month;
+    this.monthChange.next(month);
+  }
+
+  nextMonth(): void {
+    this.onMonthSelected(this.displayedMonth);
   }
 }
